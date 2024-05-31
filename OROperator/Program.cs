5317e6c63f3b4effaae391d98bca5245ec27529a
= <=>
2024-06-01 00:57:57 +0300

    week5

---
+++ OROperator/Program.cs
@@ -0,0 +1,19 @@
+﻿int math, biology, chemistry;
+
+Console.WriteLine("Enter math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (biology >= 90 && chemistry >= 90))
+{
+    Console.WriteLine("You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
